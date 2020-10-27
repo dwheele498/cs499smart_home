c665550e81cf1d7225dda4c5e9016083de9a01ba
Dylan Wheeler <dwheel7@uab.edu>
2020-10-26 22:49:12 -0500

    updated store and admin panel with events to trigger power and water simulations. added a timer package because im too lazy to do it in vanilla js

--- smarthome/src/store/index.ts
+++ smarthome/src/store/index.ts
@@ -4,18 +4,34 @@ import Vuex, { MutationTree } from "vuex";
 Vue.use(Vuex);
 
 export interface RootState {
-  index: number;
+  index?: number;
+  power: number;
+  water: number;
 }
 
 const mutations: MutationTree<RootState> = {
   resetIndex(state) {
     state.index = 0;
   },
+  addPower(state, payload: number) {
+    state.power += payload;
+  },
+  addWater(state, payload: number) {
+    state.water += payload;
+  },
+  resetPower(state){
+    state.power = 0;
+  },
+  resetWater(state){
+    state.water = 0;
+  }
 };
 
 const store = new Vuex.Store<RootState>({
   state: {
-    index: 0,
+    index: undefined,
+    power: 0,
+    water: 0,
   },
   mutations: mutations,
 });
--- smarthome/src/views/AdminPanel.vue
+++ smarthome/src/views/AdminPanel.vue
@@ -3,32 +3,36 @@
     <v-container fluid>
       <v-row>
         <v-banner>Room Control</v-banner>
-        <v-col v-for="room in rooms" :key="room">
-          <v-switch color="orange" :label="room"></v-switch>
+        <v-col v-for="room in rooms" :key="room + ' ' + 'power'">
+          <v-switch
+            @change="firePowerEvent($event)"
+            color="orange"
+            :label="room"
+          ></v-switch>
         </v-col>
       </v-row>
       <v-row>
         <v-banner>Door Control</v-banner>
-        <v-col v-for="room in rooms" :key="room + ' ' + door">
-          <v-switch color="red" :label="room"></v-switch>
+        <v-col v-for="room in rooms" :key="room + ' ' + 'power'">
+          <v-switch @change="firePowerEvent($event)" color="red" :label="room"></v-switch>
         </v-col>
       </v-row>
       <v-row>
         <v-banner>Light Control</v-banner>
-        <v-col v-for="room in rooms" :key="room + ' ' + door">
-          <v-switch color="yellow" :label="room"></v-switch>
+        <v-col v-for="room in rooms" :key="room + ' ' + 'power'">
+          <v-switch @change="firePowerEvent($event)" color="yellow" :label="room"></v-switch>
         </v-col>
       </v-row>
       <v-row>
         <v-banner>Water Control</v-banner>
-        <v-col v-for="wat in water" :key="wat">
-          <v-switch color="teal darken-4" :label="wat"></v-switch>
+        <v-col v-for="wat in water" :key="wat + ' ' + 'water'">
+          <v-switch @change="fireWaterEvent($event)" color="teal darken-4" :label="wat"></v-switch>
         </v-col>
       </v-row>
       <v-row>
         <v-banner>Power Control</v-banner>
-        <v-col cols="auto"  v-for="pow in power" :key="pow">
-          <v-switch color="amber darken-4" :label="pow"></v-switch>
+        <v-col cols="auto" v-for="pow in power" :key="pow + ' ' + 'power'">
+          <v-switch @change="firePowerEvent($event)" color="amber darken-4" :label="pow"></v-switch>
         </v-col>
       </v-row>
     </v-container>
@@ -38,22 +42,64 @@
 <script lang="ts">
 import Vue from "vue";
 import { ROOMS, POWER_DEVICES, WATER_DEVICES } from "../consts";
+import  {Timer}  from 'easytimer.js';
+import { mapMutations } from 'vuex';
 export default Vue.extend({
   data: () => ({
     rooms: [] as string[],
     power: [] as string[],
-    water: [] as string[] 
+    water: [] as string[],
+    timer: {} as Timer,
   }),
   created() {
+    this.resetPower();
+    this.resetWater();
     this.rooms = ROOMS;
     this.power = POWER_DEVICES;
     this.water = WATER_DEVICES;
-
+    this.timer = new Timer();
+  },
+  methods: {
+    ...mapMutations(['addPower','addWater','resetPower','resetWater']),
+    timerStart() {
+      this.timer.reset();
+      this.timer.start();
+      console.log('Timer start');
+    },
+    timerStop() {
+      this.timer.pause();
+    },
+    getTime(){
+      console.log('Sending time');
+      const sp = this.timer.getTimeValues().toString().split(':');
+      Number.parseInt(sp[2])<30?sp[2]='30':sp[2];
+      this.addPower(Number.parseInt(sp[2]));
+      
+    },
+    getWaterTime(){
+      this.addWater(1);
+    },
+    firePowerEvent(event: any){
+      console.log(event);
+      if(event===true){
+        this.timerStart();
+      } 
+      else{
+        this.timerStop();
+        this.getTime();
+      }
+    },
+    fireWaterEvent(event: any){
+           if(event===true){
+        this.timerStart();
+      } 
+      else{
+        this.timerStop();
+        this.getWaterTime();
+      }
+    }
   },
 });
 </script>
 
 <style></style>
-
-
-1 blahblahflsdsldhdkjfhf;ksdhf 2019 sjkfhsdlkfhdfn 

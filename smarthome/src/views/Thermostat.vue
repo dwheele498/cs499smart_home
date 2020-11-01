91676a6b4c872c104d39a40f94bdc22bab2a3bd6
Dylan Wheeler <dwheel7@uab.edu>
2020-11-01 10:50:51 -0600

    no message

--- smarthome/src/views/Thermostat.vue
+++ smarthome/src/views/Thermostat.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card raised class="mx-auto my-10" max-width="200" height="6rem">
+  <v-card v-if="callCompleted" raised class="mx-auto my-10" max-width="200" height="6rem">
     <h2 class="text-center">{{ date.toFormat("D") }}</h2>
       <h2 class="mx-auto px-auto lime-text text-center">
         Avg: {{ temp }}&#8457;
@@ -12,11 +12,12 @@ import Vue from "vue";
 import { weatherApi } from "../services/WeatherApi";
 import { API_KEY, WEATHER_STATS } from "../consts";
 import { DateTime } from "luxon";
-import { HourlyWeatherModel } from "services/models";
+import { HourlyWeatherModel } from "../services/models"
 export default Vue.extend({
   data: () => ({
     date: DateTime.local(),
     temp: 0,
+    callCompleted: false
   }),
   created() {
     this.initialize();
@@ -32,6 +33,7 @@ export default Vue.extend({
           this.temp = Number.parseInt(
             (res.data.data[0].temp * 1.8 + 32).toFixed(0)
           );
+          this.callCompleted = true
         });
     },
   },

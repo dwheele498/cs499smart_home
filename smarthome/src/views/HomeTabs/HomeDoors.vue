e091a3d1b0cec004636318fcb1ec55a8cb02cdaa
Andrew McCormick <amccorm2>
2020-11-23 16:38:30 -0600

    Floorplan is now clickable. Will add icons to floorplan in future to make clickable areas obvious.

--- smarthome/src/views/HomeTabs/HomeDoors.vue
+++ smarthome/src/views/HomeTabs/HomeDoors.vue
@@ -1,7 +1,14 @@
 <template>
-<v-img src="../../assets/homeicons/FloorPlan.svg" align-center>
-<v-container fill-height align-center class="my-6">
-<v-row>
+    <v-container fill-height align-center class="my-6">
+      <v-flex min-width="600px">
+        <v-img src="../../assets/homeicons/FloorPlan.svg"
+        width="600px"
+        max-width="100%"
+        @click="doorSwitch($event)"
+        >
+        </v-img>
+      </v-flex>
+      <v-row>
         <v-col v-for=" k of Object.keys(doors)" :key="k" cols="12" md="2">
           <v-banner class="flex-grow-1 text-left"
           height="60"
@@ -28,9 +35,8 @@
             </v-card>
           </v-item>
         </v-col>
-</v-row>
-</v-container>
-  </v-img>
+      </v-row>
+    </v-container>
 </template>
 
 <script lang="ts">
@@ -45,7 +51,32 @@ export default Vue.extend({
     ...mapState(['doors'])
   },
   methods:{
-    ...mapMutations(['openCloseDoor'])
+    ...mapMutations(['openCloseDoor']),
+    doorSwitch(event: MouseEvent){
+    console.log(event.layerX);
+    console.log(event.layerY)
+    if (event.layerX>=439 && event.layerX<560 && event.layerY<=129 && event.layerY>16){
+      this.openCloseDoor("Master Bedroom Door");
+      }else if(event.layerX>=374 && event.layerX<420 && event.layerY<=68 && event.layerY>16){
+        this.openCloseDoor("Bathroom Door 1");
+      }else if(event.layerX>=374 && event.layerX<420 && event.layerY<=133 && event.layerY>77){
+        this.openCloseDoor("Bathroom Door 2");
+      }else if(event.layerX>=398 && event.layerX<476 && event.layerY<=300 && event.layerY>199){
+        this.openCloseDoor("Kid's Bedroom 1 Door");
+      }else if(event.layerX>=515 && event.layerX<590 && event.layerY<=300 && event.layerY>199){
+        this.openCloseDoor("Kid's Bedroom 2 Door");
+      }else if(event.layerX>=234 && event.layerX<362 && event.layerY<=307 && event.layerY>169){
+        this.openCloseDoor("Front Door");
+      }else if(event.layerX>=204 && event.layerX<362 && event.layerY<=80 && event.layerY>12){
+        this.openCloseDoor("Back Door");
+      }else if(event.layerX>=10 && event.layerX<97 && event.layerY<=304 && event.layerY>215){
+        this.openCloseDoor("Garage Door 1");
+      }else if(event.layerX>=98 && event.layerX<215 && event.layerY<=304 && event.layerY>215){
+        this.openCloseDoor("Garage Door 2");
+      }else if(event.layerX>=151 && event.layerX<257 && event.layerY<=160 && event.layerY>81){
+        this.openCloseDoor("House to Garage Door");
+      }
+  },
   }
 })
 </script>
--- smarthome/src/views/HomeTabs/HomeLights.vue
+++ smarthome/src/views/HomeTabs/HomeLights.vue
@@ -1,7 +1,13 @@
 <template>
-  <v-img src="../../assets/homeicons/FloorPlan.svg" align-center>
-    
-    <v-container fill-height align-center class="my-6">
+    <v-container>
+      <v-flex min-width="600px">
+        <v-img src="../../assets/homeicons/FloorPlan.svg"
+        width="600px"
+        max-width="100%"
+        @click="lightSwitch($event)"
+        >
+        </v-img>
+      </v-flex>
       <v-row>
       <v-col v-for="k in Object.keys(lights)" :key="k" md="2">
         <v-banner class="flex-grow-1 text-left"
@@ -29,15 +35,14 @@
           </v-item>
         </v-col>
       </v-row>
-    </v-container>
-  </v-img>
-  
+    </v-container>  
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { mapMutations, mapState } from "vuex";
 import { ROOMS } from "../../consts";
+
 export default Vue.extend({
   data: () => ({
     rooms: ROOMS,
@@ -47,8 +52,40 @@ export default Vue.extend({
   },
   methods: {
     ...mapMutations(["onOffLight"]),
+    lightSwitch(event: MouseEvent){
+    console.log(event.layerX);
+    console.log(event.layerY)
+    if (event.layerX>=439 && event.layerX<560 && event.layerY<=129 && event.layerY>16){
+      this.onOffLight("Master Bedroom Overhead");
+      }else if(event.layerX>=561 && event.layerX<591 && event.layerY<=43 && event.layerY>19){
+        this.onOffLight("MB Lamp 1");
+      }else if(event.layerX>=561 && event.layerX<591 && event.layerY<=129 && event.layerY>102){
+        this.onOffLight("MB Lamp 2");
+      }else if(event.layerX>=374 && event.layerX<420 && event.layerY<=68 && event.layerY>16){
+        this.onOffLight("Bathroom 1 Overhead");
+      }else if(event.layerX>=374 && event.layerX<420 && event.layerY<=133 && event.layerY>77){
+        this.onOffLight("Bathroom 2 Overhead");
+      }else if(event.layerX>=398 && event.layerX<476 && event.layerY<=300 && event.layerY>199){
+        this.onOffLight("Kid 1 Overhead");
+      }else if(event.layerX>=374 && event.layerX<397 && event.layerY<=300 && event.layerY>199){
+        this.onOffLight("Kid 1 Lamp 1");
+      }else if(event.layerX>=515 && event.layerX<590 && event.layerY<=300 && event.layerY>199){
+        this.onOffLight("Kid 2 Overhead");
+      }else if(event.layerX>=477 && event.layerX<514 && event.layerY<=300 && event.layerY>199){
+        this.onOffLight("Kid 2 Lamp 2");
+      }else if(event.layerX>=234 && event.layerX<362 && event.layerY<=307 && event.layerY>169){
+        this.onOffLight("Living Room Overhead");
+      }else if(event.layerX>=204 && event.layerX<233 && event.layerY<=304 && event.layerY>238){
+        this.onOffLight("LVR Lamp 1");
+      }else if(event.layerX>=204 && event.layerX<233 && event.layerY<=239 && event.layerY>168){
+        this.onOffLight("LVR Lamp 2");
+      }else if(event.layerX>=204 && event.layerX<362 && event.layerY<=160 && event.layerY>12){
+        this.onOffLight("Kitchen Overhead");
+      }
+  },
   },
 });
+
 </script>
 
 <style></style>

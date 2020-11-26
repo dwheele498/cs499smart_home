eee1433bec47a5267077e04afbac58e40dd05ded
Dylan Wheeler <dwheel7@uab.edu>
2020-11-25 20:20:07 -0600

    added manual controls for power events

--- smarthome/src/views/AdminPanel.vue
+++ smarthome/src/views/AdminPanel.vue
@@ -4,6 +4,47 @@
       <v-expansion-panels accordion>
         <v-expansion-panel>
           <v-expansion-panel-header>
+            <v-banner>Manual Controls</v-banner>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-row class="justify-center">
+              <v-col>
+                <v-radio-group>
+                  <v-radio
+                    @change="radioPick(5)"
+                    color="success"
+                    label="5"
+                    value="5"
+                  >
+                  </v-radio>
+                  <v-radio
+                    @change="radioPick(10)"
+                    color="success"
+                    label="10"
+                    value="10"
+                  >
+                  </v-radio>
+                  <v-radio
+                    @change="radioPick(15)"
+                    color="success"
+                    label="15"
+                    value="15"
+                  >
+                  </v-radio>
+                </v-radio-group>
+              </v-col>
+              <v-col>
+                <v-select :items="power" v-model="appliance" clearable label="Appliance Name"></v-select>
+                <v-btn @click="manualSubmit()" color="success" rounded>Submit</v-btn>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+
+      <v-expansion-panels accordion>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
             <v-banner
               ><v-icon class="material-icons">house</v-icon>Rooms</v-banner
             >
@@ -37,7 +78,6 @@
                   @change="openCloseDoor(door)"
                   color="red"
                   :label="door"
-                  
                 ></v-switch>
               </v-col>
             </v-row>
@@ -104,7 +144,7 @@
                 :key="pow + ' ' + 'power'"
               >
                 <v-switch
-                  @change="firePowerEvent($event,pow)"
+                  @change="firePowerEvent($event, pow)"
                   color="amber darken-4"
                   :label="pow"
                   :input-value="$store.state.power[pow].on"
@@ -115,6 +155,16 @@
         </v-expansion-panel>
       </v-expansion-panels>
     </v-container>
+    <v-snackbar v-model="submit">Successfully Submitted Manual Operation <template v-slot:action="{ attrs }">
+        <v-btn
+          color="green"
+          text
+          v-bind="attrs"
+          @click="submit = false"
+        >
+          Close
+        </v-btn>
+      </template></v-snackbar>
   </v-main>
 </template>
 
@@ -123,9 +173,9 @@ import Vue from "vue";
 import { ROOMS, POWER_DEVICES, WATER_DEVICES } from "../consts";
 import { Timer } from "easytimer.js";
 import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
-import { powerApi } from '@/services/PowerApi';
-import store from '@/store';
-import { waterApi } from '@/services/WaterApi';
+import { powerApi } from "@/services/PowerApi";
+import store from "@/store";
+import { waterApi } from "@/services/WaterApi";
 export default Vue.extend({
   data: () => ({
     rooms: [] as string[],
@@ -134,21 +184,41 @@ export default Vue.extend({
     timer: new Timer(),
     doors: [] as string[],
     lights: [] as string[],
+    radio: 0,
+    appliance: "",
+    submit: false,
   }),
   created() {
     this.rooms = ROOMS;
     this.power = POWER_DEVICES;
-    console.log(this.power)
+    console.log(this.power);
     this.water = WATER_DEVICES;
     for (const [k, v] of Object.entries(this.$store.state.doors)) {
       this.doors.push(k);
     }
-    for (const [k,v] of Object.entries(this.$store.state.lights)) {
+    for (const [k, v] of Object.entries(this.$store.state.lights)) {
       this.lights.push(k);
     }
   },
   methods: {
-    ...mapMutations(["addPower", "addWater","powerSwitch","waterSwitch","openCloseDoor","onOffLight"]),
+    ...mapMutations([
+      "addPower",
+      "addWater",
+      "powerSwitch",
+      "waterSwitch",
+      "openCloseDoor",
+      "onOffLight",
+    ]),
+    radioPick(event: any) {
+      this.radio = Number.parseInt(event);
+    },
+    manualSubmit(){
+      this.addPower([this.appliance,this.radio]);
+      this.submit = true;
+      this.appliance = '';
+
+
+    },
     getTime(): number {
       console.log("Sending time");
       const sp = this.timer
@@ -169,15 +239,15 @@ export default Vue.extend({
         this.addPower([name, this.getTime()]);
         const pow = this.$store.state.power[name].amt;
         name = name.toLowerCase();
-        switch(name){
+        switch (name) {
           case "livetv":
-            powerApi.sendPower('livingtv',pow);
+            powerApi.sendPower("livingtv", pow);
             break;
           case "bathexhaust":
-            powerApi.sendPower('exhaust',pow);
+            powerApi.sendPower("exhaust", pow);
             break;
           default:
-            powerApi.sendPower(name,pow);
+            powerApi.sendPower(name, pow);
             break;
         }
         this.timer.stop();
@@ -194,14 +264,14 @@ export default Vue.extend({
         this.addWater([name, this.getTime()]);
         const wat = this.$store.state.water[name].amt;
         name = name.toLowerCase();
-        waterApi.sendWater(name,wat);
+        waterApi.sendWater(name, wat);
         this.timer.stop();
       }
     },
-    lightSwitch(name: string){
+    lightSwitch(name: string) {
       console.log(name);
       this.onOffLight(name);
-    }
+    },
   },
 });
 </script>
